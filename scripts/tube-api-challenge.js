4a448665fea10b27617f329496a23a1db2d9fa4a
Simon Chae <s13chae@gmail.com>
2018-06-19 12:31:25 -0700

    finished required steps of Youtube API project

--- scripts/tube-api-challenge.js
+++ scripts/tube-api-challenge.js
@@ -1,26 +1,48 @@
 'use strict'; 
 /* global $ */
 
+const API_KEY = 'AIzaSyDdjaDtO9Q7xxkeLIWSnCXnyY4TFGwsuWs';
+
 const YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search';
 
-function getDataFromApi(searchTerm, callback){
+const getDataFromApi = function(searchTerm, callback) {
   const query = {
-    q: `${searchTerm}`,
-    key: 'AIzaSyDdjaDtO9Q7xxkeLIWSnCXnyY4TFGwsuWs',
+    q: searchTerm,
+    key: API_KEY,
     part: 'snippet',
-    maxResults: 25
+    maxResults: 25,
   };
-  $.getJSON(YOUTUBE_URL,query,(response) =>{
-    console.log(response);
-  });
-}
-
-let test = function(){
-  console.log('alala');
+  $.getJSON(YOUTUBE_URL,query, callback);
 };
 
-console.log(getDataFromApi('a', test));
-// function renderResults(result){
+const renderResults = function(result){
+  const youtubePrefix = 'https://www.youtube.com/watch?v=';
+  return `
+    <div>
+      <h2>
+      <a class="js-result-name" href="${youtubePrefix}${result.id.videoId}" target="_blank">${result.snippet.title}</a> 
+      <a class="js-result-name" href="${youtubePrefix}${result.id.videoId}" target="_blank"><p><img src="${result.snippet.thumbnails.medium.url}" alt="IMAGE"></p></a> 
+    </div>
+  `;
+};
+
+const displayYoutubeSearchData = function(data) {
+  const results = data.items.map((item) => renderResults(item));
+  $('.js-search-results').html(results);
+};
+
+const watchSubmit = function() {
+  $('.js-search-form').submit(event => {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    queryTarget.val('');
+    console.log(query);
+    getDataFromApi(query, displayYoutubeSearchData);
+  });
+};
+
+$(watchSubmit);
 
 
 

30e22de1a3955ab8268458566e96a58ca8ddc052
ccareyncsu <ccarey@ncsu.edu>
2018-06-19 12:33:32 -0600

    moved js file

---
+++ scripts/tube-api-challenge.js
@@ -0,0 +1,26 @@
+'use strict'; 
+/* global $ */
+
+const YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search';
+
+function getDataFromApi(searchTerm, callback){
+  const query = {
+    q: `${searchTerm}`,
+    key: 'AIzaSyDdjaDtO9Q7xxkeLIWSnCXnyY4TFGwsuWs',
+    part: 'snippet',
+    maxResults: 25
+  };
+  $.getJSON(YOUTUBE_URL,query,(response) =>{
+    console.log(response);
+  });
+}
+
+let test = function(){
+  console.log('alala');
+};
+
+console.log(getDataFromApi('a', test));
+// function renderResults(result){
+
+
+

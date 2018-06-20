6432b5390babaae9b05b34398d463eeba815c6b4
ccareyncsu <ccarey@ncsu.edu>
2018-06-19 20:06:58 -0600

    added store object to access info

--- scripts/tube-api-challenge.js
+++ scripts/tube-api-challenge.js
@@ -10,7 +10,7 @@ const getDataFromApi = function(searchTerm, callback) {
     q: searchTerm,
     key: API_KEY,
     part: 'snippet',
-    maxResults: 25,
+    maxResults: 5,
   };
   $.getJSON(YOUTUBE_URL,query, callback);
 };
@@ -37,12 +37,29 @@ const displayYoutubeSearchData = function(data) {
   console.log(data);
   const youtubePrefix = 'https://www.youtube.com/watch?v=';
   const results = data.items.map((item) => renderResults(item));
-
-  const storeItems = data.items.map( (item) => console.log(item));
-
   $('.js-search-results').html(results);
+
+  const indexItems = data.items.map((item) => {
+    return {
+      id: item.id.videoId,
+      title: item.snippet.title,
+      thumbnail: item.snippet.thumbnails.default.url,
+      channelId: item.snippet.channelId,
+      channelTitle: item.snippet.channelTitle,
+    };
+  });
+  console.log(indexItems);
   
-  console.log(storeItems);
+  
+  // const indexItems = data.items.map((item) => {
+  //   return item;
+  // });
+  // console.log(indexItems);
+  // const storeItems = indexItems.map(index => {
+  //   console.log(index);
+  //   store.arr.push({
+      
+  // });
   
 };
 
@@ -53,13 +70,17 @@ const thumbnailClick = function(){
     const youtubePrefix = 'https://www.youtube.com/embed/';
     const currentVidId = $(event.currentTarget).attr('data-video-id');
     $('.modal-content').html(`<iframe width="420" height="315" src="${youtubePrefix}${currentVidId}?autoplay=1"> </iframe>`);
-    $('.modal').show();
+    $('.modal').fadeIn();
   });
 };
 
 const modalExitClick = function(){
-  
-}
+  $('.cursor').click(event => {
+    console.log('modal exit ran');
+    $('.modal').fadeOut();
+    $('.modal-content').children('iframe').attr('src', '');
+  });
+};
 
 
 const watchSubmit = function() {
@@ -76,7 +97,8 @@ const watchSubmit = function() {
 const runFunction = function(){
   thumbnailClick();
   watchSubmit();
-}
+  modalExitClick();
+};
 
 $(runFunction);
 
